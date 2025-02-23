c5b01f5693467c511aed2d81ba31ff08363dd9ce
William Li <52115161+Trollermaner@users.noreply.github.com>
2025-02-23 15:38:00 -0500

    valid parantheses

---
+++ python/easy/20. Valid Parantheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        s only has ()[]{}
+        solution is to have a stack that keeps count of all open parantheses.
+        every time we loop over a closing paranthese, we check if the top of the stack
+        holds the corresponding opening paranthese
+        """
+
+        open_brackets = ["[", "(", "{"]
+        corr_brackets = {
+            "]": "[",
+            ")": "(",
+            "}": "{",
+        }
+        stack = []
+
+        for x in s:
+            if x in open_brackets:
+                stack.append(x)
+
+            else:
+                if stack and stack[-1] == corr_brackets[x]:
+                    stack.pop()
+                else:
+                    return False
+
+        # we only return True if stack is empty (all brackets close)
+        return False if stack else True
